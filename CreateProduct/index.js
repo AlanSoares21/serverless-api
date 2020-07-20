2447ab5293309cb76403edce2eadbc467ce1e22e
AlanSoares21 <alansoares26@outlook.com>
2020-07-20 19:38:03 -0300

    funcao create product

---
+++ CreateProduct/index.js
@@ -0,0 +1,12 @@
+const createMongoClient = require('../shared/mongoClient');
+module.exports = async function (context, req) {
+    const product = req.body;
+    const { client: MongoClient, closeConnectionFn } = await createMongoClient();
+    const Products = MongoClient.collection('products');
+    const body = await Products.insert(product);
+    closeConnectionFn();
+    context.res = {
+        status: 201, 
+        body,
+    };
+}
